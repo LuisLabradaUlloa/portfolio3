8fd40682b94fac891a668822cd4429041dedd805
LuisLabradaUlloa <luis_ulloa@estudante.sesisenai.org.br>
2024-11-13 12:26:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+function apresentacao() {
+    window.location.href = 'apresentacao.html';
+    }
+
+function voltar() {
+    window.location.href = 'index.html';
+    }
+
+function curriculum() {
+    window.location.href = 'curriculum.html'
+}
+
+function certificado(){
+    window.location.href = 'certificados.html'
+}
+
+function natureza(){
+    window.location.href = 'natureza.html'
+}
+
+function humanas(){
+    window.location.href = 'humanas.html'
+}
+
+function matematica(){
+    window.location.href = 'matematica.html'
+}
+
+function linguagens(){
+    window.location.href = 'linguagens.html'
+}
+
+function tecnico(){
+    window.location.href = 'tecnico.html'
+}
+
+function humanas1(){
+    window.location.href = 'atividadesHumanas1.html'
+}
+
+function humanas2(){
+    window.location.href = 'atividadesHumanas2.html'
+}
+
+function humanas3(){
+    window.location.href = 'atividadesHumanas3.html'
+}
+
+function voltarHumanas(){
+    window.location.href = 'humanas.html'
+}
+
+function linguagens1(){
+    window.location.href = 'atividadesLinguagens1.html'
+}
+
+function linguagens2(){
+    window.location.href = 'atividadesLinguagens2.html'
+}
+
+function linguagens3(){
+    window.location.href = 'atividadesLinguagens3.html'
+}
+
+function voltarLing(){
+    window.location.href = 'linguagens.html'
+}
+
+function matematica1(){
+    window.location.href = 'atividadesMat1.html'
+}
+
+function matematica2(){
+    window.location.href = 'atividadesMat2.html'
+}
+
+function matematica3(){
+    window.location.href = 'atividadesMat3.html'
+}
+
+function voltarMat(){
+    window.location.href = 'matematica.html'
+}
+
+function natureza1(){
+    window.location.href = 'atividadesNatu1.html'
+}
+
+function natureza2(){
+    window.location.href = 'atividadesNatu2.html'
+}
+
+function natureza3(){
+    window.location.href = 'atividadesNatu3.html'
+}
+
+function voltarNatu(){
+    window.location.href = 'natureza.html'
+}
+
+function PPE(){
+    window.location.href = 'PPE.html'
+}
